278937afe6d982b8250747ba98a8acad10aee5f5
cop20skj <92833180+cop20skj@users.noreply.github.com>
2021-11-02 02:26:04 +0000

    Mon - updates

--- task2_logic1.c
+++ task2_logic1.c
@@ -23,7 +23,7 @@
 #define obj_max 30		//value if obj is farther than detectable range by prox sensor
 #define obj_min 1000	//value if obj is closer than detectable range by prox sensor
 #define wait 50			//duration for which to pause the robot
-#define dist_min 15		//value if obj is closer than detectable range by distance sensor
+#define dist_min 20		//value if obj is closer than detectable range by distance sensor
 #define dist_max 500	//value if obj is farther than detectable range by distance sensor
 
 // defining inter process communication bus
@@ -32,8 +32,8 @@ MUTEX_DECL(bus_lock);
 CONDVAR_DECL(bus_condvar);
 
 int prox_cal[8]={0,0,0,0,0,0,0,0};	//For storing proximity sensor values
-int i=0;
-int prox_max=0;			//For dectecting prox sensor with max value
+int i;
+int prox_max;			//For dectecting prox sensor with max value
 int prox_loc;			//Prox sensor with the max value i.e the sensor to which the obj is closest
 //int prox_avg;
 int dist;				//Distance measured by the distance sensor
@@ -69,7 +69,7 @@ int main(void)
     while (1)
     {
     	chThdSleepMilliseconds(wait);
-
+    	prox_max = 0;
         //Getting the proximity sensor values
         for(i=0;i<8;++i)
         {
@@ -80,59 +80,92 @@ int main(void)
         		prox_loc = i;
         	}
         }
-        //prox_avg = (prox_cal[0]+prox_cal[7])/2);
         // If obj is within detectable limit and not at front of robot, rotate robot till obj is at front
+        // LHS or RHS LED will blink red
         if(((prox_max>obj_max)&&(prox_max<obj_min))&&((prox_loc!=0)&&(prox_loc!=7)));
         {
         	count=0;
         	if(prox_loc<=3)						//If obj at right, rotate right
         	{
+        		clear_leds();
+        		set_led(LED3,2);
         		left_motor_set_speed(250);
         		right_motor_set_speed(-250);
         	}
         	else								//If obj at left, rotate left
         	{
+        		clear_leds();
+        		set_led(LED7,2);
         		left_motor_set_speed(-250);
         		right_motor_set_speed(250);
         	}
         }
-        //If obj at front and at close distance, then robot moves back
-        if((prox_max>obj_near)&&((prox_loc==0)||(prox_loc==7)))
+        //If obj at front and at close distance, then the robot moves back
+        //RGB LEDs at back shows red
+        else if((prox_max>obj_near)&&((prox_loc==0)||(prox_loc==7)))
         {
         	count=0;
-        	left_motor_set_speed(-300);
-        	right_motor_set_speed(-300);
+        	clear_leds();
+        	set_rgb_led(LED4,10,0,0);
+        	set_rgb_led(LED6,10,0,0);
+        	left_motor_set_speed(-400);
+        	right_motor_set_speed(-400);
         }
-        //If obj at front and at far distance, then robot moves front
+        //If obj at front and at far distance, then the robot moves front
+        //RGB LEDs at front shows red
         else if ((prox_max<obj_far)&&((prox_loc==0)||(prox_loc==7)))
         {
         	count=0;
-        	left_motor_set_speed(300);
-        	right_motor_set_speed(300);
+        	clear_leds();
+        	set_rgb_led(LED2,10,0,0);
+        	set_rgb_led(LED8,10,0,0);
+        	left_motor_set_speed(400);
+        	right_motor_set_speed(400);
         }
         //If obj is not at detectable range i.e too close or too far, robot uses distance sensor
-        else if((prox<obj_max)&&(count<1))
+        else if((prox_max<obj_max)&&(count<1))
         {
-        	i = 0;
         	//rotate robot for approx 15sec to detect obj using distance sensor
-        	while((i<300)&&(flag==0))
+        	//All RGB LEDs show blue
+        	for(i=0;(i<300)&&(flag==0);++i)
         	{
+        		clear_leds();
+        		set_rgb_led(LED2,0,0,10);
+        		set_rgb_led(LED4,0,0,10);
+        		set_rgb_led(LED6,0,0,10);
+        		set_rgb_led(LED8,0,0,10);
         		left_motor_set_speed(250);
         		right_motor_set_speed(-250);
         		dist = VL53L0X_get_dist_mm();
-        		if((dist>dist_min)&&(dist<dis_max))
-        			flag=1;
         		chThdSleepMilliseconds(wait);
+        		if((dist>dist_min)&&(dist<dist_max))
+        			flag=1;
         	}
-        	count+=1;
-        	if (flag==1)
+        	if(flag==0)
+        		count+=1;
+        	//If robot detects obj and while its in range,  robot moves forward
+        	//All RGB LEDs shows green
+        	else
         	{
-        		left_motor_set_speed(500);
-        		right_motor_set_speed(500);
+        		while((dist>dist_min)&&(dist<dist_max))
+        		{
+        			clear_leds();
+        			set_rgb_led(LED2,0,10,0);
+        			set_rgb_led(LED4,0,10,0);
+        			set_rgb_led(LED6,0,10,0);
+        			set_rgb_led(LED8,0,10,0);
+        			left_motor_set_speed(500);
+        			right_motor_set_speed(500);
+        			dist = VL53L0X_get_dist_mm();
+        			chThdSleepMilliseconds(wait);
+        		}
         	}
         }
+        //If obj is in the proper position, robot stays still
+        //All LEDs OFF
         else
         {
+        	clear_leds();
         	left_motor_set_speed(0);
         	right_motor_set_speed(0);
         }
